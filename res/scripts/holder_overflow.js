9202b52bb85c3d44a321ec3486d78861a9251ac8
timtim17 <cheyvjenkins12345@gmail.com>
2014-01-13 18:11:51 -0800

    Re-add Refrences to Online Resources

--- res/scripts/holder_overflow.js
+++ res/scripts/holder_overflow.js
@@ -4,6 +4,6 @@ function OverflowHF(){
         deck.cards = this.cards;
     }
     this.addACard = function(card){
-        // DO SOMETHING WITH CARD
+        this.cards[this.cards.length] = card;
     }
 }
--- res/scripts/stack.js
+++ res/scripts/stack.js
@@ -1,14 +1,14 @@
-function Stack(defaultTopCard){
+function Stack(defaultTopCard) {
     this.cards = [defaultTopCard];
-    
-    this.createStack = function(stackNumber){
+
+    this.createStack = function(stackNumber) {
         var button = document.createElement('button');
         button.setAttribute('type', 'button');
         var img = document.createElement('img');
         img.setAttribute('src', defaultTopCard.img);
         img.setAttribute('width', 100);
         img.setAttribute('height', 150);
-        
+
         button.appendChild(img);
         document.getElementById('cards').appendChild(button);
         return img;
